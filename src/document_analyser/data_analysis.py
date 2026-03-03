5e7ca7bd2455420492181edd462298dacd62d7c3
miku acharya <mikuacharya3@gmail.com>
2026-03-03 17:25:24 +0530

    document analyzer created

---
+++ src/document_analyser/data_analysis.py
@@ -0,0 +1,17 @@
+import os
+from utils.model_loader import ModelLoader
+from logger.custom_logger import CustomLogger
+from exception.custom_exception import DocumentPortalException
+from model.models import *
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.output_parsers import OutputFixingPrser
+
+class DocumentAnalyzer:
+    """
+    Analyzes documents using a pre-trained model.
+    Automatically logs all actions and supports session-based organization.
+    """
+    def __init__(self):
+        pass
+    def analyze_metadata(self):
+        pass
