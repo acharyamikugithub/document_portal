4209a100b51efb2e6245e4dfac65b6f2d975cb57
miku acharya <mikuacharya3@gmail.com>
2026-03-09 23:20:01 +0530

    code updated for reading pdf

--- src/document_compare/data_ingestion.py
+++ src/document_compare/data_ingestion.py
@@ -4,20 +4,47 @@ import fitz
 from logger.custom_logger import CustomLogger
 from exception.custom_exception import DocumentPortalException
 
-class DocumentComparator:
-    def __init__(self):
-        pass
+class DocumentIngestion:
+    def __init__(self,base_dir):
+        self.log=CustomLogger().get_logger(__name__)
+        self.base_dir=Path(base_dir)
+        self.base_dir.mkdir(parents=True,exist_ok=True)
     def delete_existing_files(self):
         """
         Deletes existing files at the specified paths
         """
+        try:
+            pass
+        except Exception as e:
+            self.log.error(f"Error deleting existing files: {e}")
+            raise DocumentPortalException("An error occured while deleeting existing files",sys)
     def save_iploaded_files(self):
         """
         Saves uploaded fies to a specific directory.
         """
-        pass
-    def read_pdf(self):
+        try:
+            pass
+        except Exception as e:
+            self.log.error(f"Error saving uploaded files: {e}")
+            raise DocumentPortalException("An error occured while saving uploaded files",sys)
+    def read_pdf(self,pdf_path:Path)->str:
         """
         Reads a PDF file and extract text from each page.
         """
-        pass
+        try:
+            with fitz.open(pdf_path) as doc:
+                if doc.is_encrypted:
+                    raise ValueError(f"PDF is encrypted: {pdf_path.name}")
+                all_text=[]
+                for page_num in range(doc.page_count):
+                    doc.load_page(page_num)
+                    text=page.get_text()#type: ignore
+                    if text.strip():
+                        all_text.append(f"\n---Page{page_num+1}---\n{text}")
+                    self.log.info("PDF read successfully",file=str(pdf_path),pages=len(all_text))
+                    return "\n".join(all_text)
+
+        except Exception as e:
+            self.log.error(f"Erro reading PDF: {e}")
+            raise DocumentPortalException("An error occured while reading the PDF.",sys)
+
