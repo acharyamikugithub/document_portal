a52055f8ea1718302179c5729fd56aee264cb6fa
miku acharya <mikuacharya3@gmail.com>
2026-04-21 20:21:19 +0530

    singledoc chat code updated

--- prompt/prompt_library.py
+++ prompt/prompt_library.py
@@ -1,4 +1,4 @@
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate,MessagesPlaceholder
 
 docuement_analysis_prompt= ChatPromptTemplate.from_template("""
 You are a highly value capable assistant trained to analyze and summarize documents.
@@ -24,5 +24,25 @@ Your response should follow this format:
 
 {format_instruction}
 """)
+contextualize_question_prompt = ChatPromptTemplate.from_messages([
+    ("system", (
+        "Given a conversation history and the most recent user query, rewrite the query as a standalone question "
+        "that makes sense without relying on the previous context. Do not provide an answer—only reformulate the "
+        "question if necessary; otherwise, return it unchanged."
+    )),
+    MessagesPlaceholder("chat_history"),
+    ("human", "{input}"),
+])
 
-PROMPT_REGISTRY={"document_analysis":docuement_analysis_prompt,"document_comparison":document_comparison_prompt}
+# Prompt for answering based on context
+context_qa_prompt = ChatPromptTemplate.from_messages([
+    ("system", (
+        "You are an assistant designed to answer questions using the provided context. Rely only on the retrieved "
+        "information to form your response. If the answer is not found in the context, respond with 'I don't know.' "
+        "Keep your answer concise and no longer than three sentences.\n\n{context}"
+    )),
+    MessagesPlaceholder("chat_history"),
+    ("human", "{input}"),
+])
+
+PROMPT_REGISTRY={"document_analysis":docuement_analysis_prompt,"document_comparison":document_comparison_prompt,"contextualize_question":contextualize_question_prompt,"context_qa":context_qa_prompt}
--- src/single_document_chat/retrieval.py
+++ src/single_document_chat/retrieval.py
@@ -14,15 +14,38 @@ from prompt.prompt_library import PROMPT_REGISTRY
 from model.models import PromptType
 
 class ConversationalRAG:
-    def __init__(self,session_id,retriever) -> None:
+    def __init__(self,session_id,retriever):
         try:
             self.log=CustomLogger().get_logger(__name__)
+            self.session_id=session_id
+            self.retriever=retriever
+            self.llm=self._load_llm
+            self.contextualize_prompt=PROMPT_REGISTRY[PromptType.CONTEXTUALIZE_QUESTION.value]
+            self.qa_prompt=PROMPT_REGISTRY[PromptType.CONTEXT_QA.value]
+            self.history_aware_retriever=create_history_aware_retriever(
+                    self.llm,self.retriever,self.contextualize_prompt
+                )   
+            self.log.info("crated history aware retriever successfully",session_id=session_id)
+            self.qa_chain=create_stuff_documents_chain(self.llm,self.qa_prompt)
+            self.rag_chain=create_retrieval_chain(self.history_aware_retriever,self.qa_chain)
+            self.log.info("Created RAG chain successfully",session_id=session_id)
+            self.chain=RunnableWithMessageHistory(
+                    self.rag_chain,
+                    self._get_session_history,
+                    input_messages_key="input",
+                    history_messages_key="chat_history",
+                    output_messages_key="answer"
+                    )
+            self.log.info("Created Runnablewithmessagehistory",session_id=session_id)
+
         except Exception as e:
             self.log.error("Error intializing ConversationRAG",error=str(e),session_id=session_id)
             raise DocumentPortalException("Failed to intialize ConversationalRAG",sys)
     def _load_llm(self):
         try:
-            pass
+            llm=ModelLoader.load_llm()
+            self.log.info("Loaded LLM successfully",class_name=self.__class__.__name__)
+            return llm
         except Exception as e:
             self.log.error("Error loading llm via ModelLoader",error=str(e))
             raise DocumentPortalException("Failed to load LLM",sys)
@@ -32,15 +55,26 @@ class ConversationalRAG:
         except Exception as e:
             self.log.error("Error retrieving session history",error=str(e))
             raise DocumentPortalException("Failed to retrieve session history",sys)
-    def load_retriever_from_faiss(self):
+    def load_retriever_from_faiss(self,index_path: str):
         try:
-            pass
+            embeddings=ModelLoader.load_embeddings()
+            if not os.path.isdir(index_path):
+                raise FileNotFoundError(f"FAISS index directory not found at {index_path}")
+            vectorestore=FAISS.load_local(index_path,embeddings)
+            self.log.info("Loaded FAISS retriever successfully from path",index_path=index_path)
+            return vectorestore.as_retriever(search_type="similarity",search_kwargs={"k":5})
+
         except Exception as e:
             self.log.error("Failed to load retriever from FAISS",error=str(e))
             raise DocumentPortalException("Error loading retriever from FAISS",sys)
-    def invoke(self):
+    def invoke(self,user_input: str)->str:
         try:
-            pass
+            response=self.chain.invoke({"input":user_input},config={"session_id":self.session_id})
+            answer=response.get("answer","No Answer")
+            if not answer:
+                self.log.warning("RAG chain returned empty answer",session_id=self.session_id)
+            self.log.info("RAG chain invoked successfully",session_id=self.session_id,user_input=user_input,answer_preview=answer[:150])
+            return answer
         except Exception as e:
             self.log.error("Failed to invoke conversational RAG",error=str(e),session_id=self.session_id)
             raise DocumentPortalException("Failed to invoke RAG chain",sys)
