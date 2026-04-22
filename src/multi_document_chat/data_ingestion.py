dd7a7a576bbc0f3109465a3dfe7008bebab0bf1e
miku acharya <mikuacharya3@gmail.com>
2026-04-22 16:04:19 +0530

    code skeleton is ready for multi doc

---
+++ src/multi_document_chat/data_ingestion.py
@@ -0,0 +1,8 @@
+class DocumentIngestor:
+    def __init__(self):
+        pass
+    def ingest(self):
+        pass
+    def _create_retriever(self,documents):
+        pass
+    
---
+++ src/multi_document_chat/retrieval.py
@@ -0,0 +1,15 @@
+class ConversationRAG:
+    def __init__(self):
+        pass
+    def load_retriever_from_faiss(self):
+        pass
+    def invoke(self):
+        pass
+    def _load_llm(self):
+        pass
+    @staticmethod
+    def _format_docs(self):
+        pass
+    def _build_lcel_chain(self):
+        pass
+
